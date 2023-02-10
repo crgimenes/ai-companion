febd59014e0526f99ccb4d47ae64bd524ca0ac5f
crgimenes <crg@crg.eti.br>
2023-02-10 00:37:07 -0300

    compile lua

--- luaengine/luaengine.go
+++ luaengine/luaengine.go
@@ -3,14 +3,14 @@ package luaengine
 import (
 	"bufio"
 	"os"
-	"text/template/parse"
 
 	lua "github.com/yuin/gopher-lua"
-	parse "github.com/yuin/gopher-lua/parse"
+	"github.com/yuin/gopher-lua/parse"
 )
 
 type LuaExtender struct {
 	luaState *lua.LState
+	proto    *lua.FunctionProto
 }
 
 func New() *LuaExtender {
@@ -22,18 +22,35 @@ func New() *LuaExtender {
 // CompileLua reads the passed lua file from disk and compiles it.
 func (le *LuaExtender) Compile(filePath string) (*lua.FunctionProto, error) {
 	file, err := os.Open(filePath)
-	defer file.Close()
 	if err != nil {
 		return nil, err
 	}
+	defer file.Close()
+
 	reader := bufio.NewReader(file)
+
 	chunk, err := parse.Parse(reader, filePath)
 	if err != nil {
 		return nil, err
 	}
+
 	proto, err := lua.Compile(chunk, filePath)
 	if err != nil {
 		return nil, err
 	}
+
 	return proto, nil
+}
+
+// DoCompiledFile takes a FunctionProto, as returned by CompileLua, and runs it in the LState. It is equivalent
+// to calling DoFile on the LState with the original source file.
+func (le *LuaExtender) DoCompiledFile(L *lua.LState, proto *lua.FunctionProto) error {
+	lfunc := L.NewFunctionFromProto(proto)
+	L.Push(lfunc)
+	return L.PCall(0, lua.MultRet, nil)
+}
+
+// InitState starts the lua interpreter with a script.
+func (le *LuaExtender) InitState() error {
+	return le.DoCompiledFile(le.luaState, le.proto)
 }
